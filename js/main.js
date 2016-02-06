db9c4c89dc52c577fed699493f6969d9d6a0de04
braby21 <braby21@gmail.com>
2016-02-06 11:46:58 -0800

    finalize for submission, adds JS for extra credit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(function() {
+  
+    var i = 1;
+
+    $('.fa-toggle-off').hide();
+
+    $('.fa').on('click',function(){
+        $(this).hide();
+        $(this).siblings().show();
+        if(i % 2) {
+            $('nav').hide();
+        } else {
+            $('nav').show();
+        }
+        i++;
+    })
+
+    var mql = window.matchMedia("(min-width: 768px)");
+
+    var handleMediaChange = function (mediaQueryList) {
+    
+        if (mediaQueryList.matches) {
+            $('.fa').hide();
+            $('nav').show();
+        }
+        else {
+            $('.fa-toggle-on').show();
+            $('nav').show();
+        }
+        
+    };
+ 
+    mql.addListener(handleMediaChange);
+    handleMediaChange(mql);
+
+}); //end document object function
